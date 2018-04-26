7eaf2e72f7d497b644ec949a96da760b22a6b2bd
Phil249 <33514603+Phil249@users.noreply.github.com>
2018-04-26 10:09:18 +0100

    Add files via upload

---
+++ make_Genome_NoHap_NoAlt_spikes_Hisat2.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+
+
+hisat2-build /home/univ3533/hg38NoAltNoHap_w_spikes.fa hg38NoAltNoHap_w_spikes_Hisat2 > output_report.log
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+"~/make_RGenome_NoHap_NoAlt_spikes_Hisat2.sh"
