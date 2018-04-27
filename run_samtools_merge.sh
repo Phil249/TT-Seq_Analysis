4a07db8ea865014ba3120ac4122933ed7ecb0a14
Phil249 <33514603+Phil249@users.noreply.github.com>
2018-04-27 09:56:07 +0100

    Add files via upload

---
+++ run_samtools_merge.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+samtools merge HEK_L_2_Minus_merged.bam HEK_L_2_Minus_1.bam HEK_L_2_Minus_2.bam &
+samtools merge HEK_L_2_Plus_merged.bam HEK_L_2_Plus_1.bam HEK_L_2_Plus_2.bam
+
+
+
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+"~/run_samtools_merge.sh"
